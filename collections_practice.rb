bef25e1912249fc43229b988e61eeac66c375090
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-15 01:31:29 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,3 +8,15 @@ def sort_array_asc(collection)
   end
 return result.flatten
 end
+#Maybe try a #map after #sort....instead of patching with an empty array, push, & #flatten?
+
+def sort_array_desc
+  i = 0
+  result = []
+  while i < collection.length
+    result = collection.sort { |a, b| b <=> a }
+binding.pry
+    i += 1
+  end
+return result.flatten
+end
