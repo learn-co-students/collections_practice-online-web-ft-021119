6e704b7c71fc2cf112ee56094a414f8fdf51769b
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-15 01:32:46 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,12 +10,11 @@ return result.flatten
 end
 #Maybe try a #map after #sort....instead of patching with an empty array, push, & #flatten?
 
-def sort_array_desc
+def sort_array_desc(collection)
   i = 0
   result = []
   while i < collection.length
     result = collection.sort { |a, b| b <=> a }
-binding.pry
     i += 1
   end
 return result.flatten
