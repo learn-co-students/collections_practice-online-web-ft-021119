d2751f116f4b065b69b222ee41ecf5585e72e386
Jacqi Entzminger <jackie.entz@gmail.com>
2019-02-17 08:30:12 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,12 @@
-
+def sort_array_asc #(25,7,1.to eq 1,7,25)
+  numbers = [25,7,1]
+  numbers.sort do |a, b|
+    if a == b 
+      0 
+      elsif a < b
+      -1
+      elsif a > b 
+      1 
+    end 
+end 
+end 
